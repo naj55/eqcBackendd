511afb8bf7fd8dba3e933536da02d715e66c76a0
najla <132200719+naj55@users.noreply.github.com>
2024-11-20 13:30:41 +0300

    Update checkToken.js

--- middleware/checkToken.js
+++ middleware/checkToken.js
@@ -2,17 +2,26 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const checkToken = (req, res, next) => {
-  const header = req.headers.authorization;
-  const token = req.headers.authorization.split(" ")[1];
+  console.log("checkToken");
+  // Check for the token in both possible locations
+  const header = req.headers.authorization || req.body.headers.Authorization;
+
+  console.log("header", header);
+
   if (!header) {
-    res.json("you must logein");
+    return res.status(401).json("You must log in");
   }
+
+  const token = header.split(" ")[1];
+
   try {
     const decoder = jwt.verify(token, process.env.secret);
     res.locals.decoder = decoder;
     next();
   } catch (err) {
-    res.status(404, "error token").json(err);
+    return res
+      .status(401)
+      .json({ error: "Invalid token", details: err.message });
   }
 };
 
