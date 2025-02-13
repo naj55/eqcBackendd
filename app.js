965e82bbbe37602f95c3e3c550294da13a8da54b
najla <132200719+naj55@users.noreply.github.com>
2025-02-13 15:06:14 +0300

    .

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const morgan = require("morgan");
 const errorHandler = require("./middleware/errorHandler");
 const path = require("path");
 
-app.use(cors());
+app.use(cors({ origin: "*", credentials: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
