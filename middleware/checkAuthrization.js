aad9f3b996846ce259dd5ee4a05972dda37d378e
najla <132200719+naj55@users.noreply.github.com>
2025-02-13 12:04:27 +0300

    .

--- middleware/checkAuthrization.js
+++ middleware/checkAuthrization.js
@@ -1,24 +1,45 @@
+// const Admin = require("../model/Admin");
+// const jwt = require("jsonwebtoken");
+// require("dotenv").config();
+// /////////checkAuthrization for student
+// const checkAuthrization = (req, res, next) => {
+//   let decoder = res.locals.decoder;
+//   const email = "eqc@aol.edu.sa";
+//   Admin.findOne({ email: email })
+//     .then((foundedUser) => {
+//       const id = foundedUser._id;
+//       useridlogedIn = decoder.result._id;
+//       if (id == useridlogedIn) {
+//         next();
+//         return;
+//       } else {
+//         res.json({ msg: "sorry you are not authorized" });
+//       }
+//     })
+//     .catch((err) => {
+//       res.status(401).json(err);
+//     });
+// };
+
+// module.exports = checkAuthrization;
+
 const Admin = require("../model/Admin");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-/////////checkAuthrization for student
-const checkAuthrization = (req, res, next) => {
-  let decoder = res.locals.decoder;
-  const email = "eqc@aol.edu.sa";
-  Admin.findOne({ email: email })
-    .then((foundedUser) => {
-      const id = foundedUser._id;
-      useridlogedIn = decoder.result._id;
-      if (id == useridlogedIn) {
-        next();
-        return;
-      } else {
-        res.json({ msg: "sorry you are not authorized" });
-      }
-    })
-    .catch((err) => {
-      res.status(401).json(err);
-    });
+
+const checkAuthrization = async (req, res, next) => {
+  const decoder = res.locals.decoder;
+
+  try {
+    // Check if user is an admin by role or specific email
+    const admin = await Admin.findOne({ email: decoder.result.email });
+    if (admin) {
+      next();
+    } else {
+      return res.status(403).json({ msg: "Unauthorized access" });
+    }
+  } catch (error) {
+    console.error("Auth Error:", error.message);
+    return res.status(500).json({ msg: "Server error", error: error.message });
+  }
 };
 
 module.exports = checkAuthrization;
--- middleware/checkToken.js
+++ middleware/checkToken.js
@@ -1,24 +1,51 @@
+// const jwt = require("jsonwebtoken");
+// require("dotenv").config();
+
+// const checkToken = (req, res, next) => {
+//   // console.log("checkToken");
+//   // Check for the token in both possible locations
+//   const header = req.headers.authorization || req.body.headers.Authorization;
+
+//   // console.log("header", header);
+
+//   if (!header) {
+//     return res.status(401).json("You must log in");
+//   }
+
+//   const token = header.split(" ")[1];
+
+//   try {
+//     const decoder = jwt.verify(token, process.env.secret);
+//     res.locals.decoder = decoder;
+//     next();
+//   } catch (err) {
+//     return res
+//       .status(401)
+//       .json({ error: "Invalid token", details: err.message });
+//   }
+// };
+
+// module.exports = checkToken;
+
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const checkToken = (req, res, next) => {
-  // console.log("checkToken");
-  // Check for the token in both possible locations
-  const header = req.headers.authorization || req.body.headers.Authorization;
-
-  // console.log("header", header);
+  const header = req.headers.authorization;
+  console.log("Authorization Header:", header);
 
   if (!header) {
     return res.status(401).json("You must log in");
   }
 
   const token = header.split(" ")[1];
-
   try {
     const decoder = jwt.verify(token, process.env.secret);
+    console.log("Token Decoded:", decoder);
     res.locals.decoder = decoder;
     next();
   } catch (err) {
+    console.error("Token verification failed:", err.message);
     return res
       .status(401)
       .json({ error: "Invalid token", details: err.message });
